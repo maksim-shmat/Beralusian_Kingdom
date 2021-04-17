afaada96cae160cb60f0d828a9e92ea71c6e7dfb
maksim-shmat <62420547+maksim-shmat@users.noreply.github.com>
2021-04-17 17:14:45 +0300

    Update settings.py

--- beralusian_kingdom_market/beralusian_kingdom_market/settings.py
+++ beralusian_kingdom_market/beralusian_kingdom_market/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pzs+e0vo6q$6=@1_hh8beyibc!!&ij$8u#^c-@859y+=3)9g$+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
